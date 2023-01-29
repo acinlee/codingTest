e20c055eb2ef2cedd20c1f4c43817ecb56800dae
DESKTOP-OD7CNTJ\dongjun <leedongjun181027@gmail.com>
2023-01-29 15:27:19 +0900

    백준 풀이 완료 - 좌표정렬하기(11650) - Arrays.sort 오름 차순 내림 차순 기억하기

---
+++ src/backjun/sorting/좌표정렬하기.java
@@ -0,0 +1,40 @@
+package backjun.sorting;
+
+import java.io.*;
+import java.util.*;
+
+public class 좌표정렬하기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int lineNum = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[lineNum][2];
+
+        for (int i=0; i<lineNum; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+
+        for (int i=0; i<lineNum; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1]);
+            bw.write("\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+
+}
