959b1f95b4d675a30bd322b3c7b4ffd281a6415c
DESKTOP-OD7CNTJ\dongjun <leedongjun181027@gmail.com>
2023-01-31 21:27:20 +0900

    백준 풀이 중 - N과M(15649번) - 블로그 참조

--- src/backjun/backtracking/N과M.java
+++ src/backjun/backtracking/N과M.java
@@ -1,15 +1,14 @@
 package backjun.backtracking;
 
-import java.io.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class N과M {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken(" "));
@@ -18,14 +17,12 @@ public class N과M {
         boolean visit[] = new boolean[N];
         int[] arr = new int[M];
         int depth = 0;
-        dfs(N, M, depth, arr);
+        dfs(N, M, depth, arr, visit);
 
-        bw.write("");
         br.close();
-        bw.close();
     }
 
-    public static void dfs(int N, int M, int depth) {
+    public static void dfs(int N, int M, int depth, int[] arr, boolean[] visit) {
         if (depth == M) {
             for (int val : arr) {
                 System.out.print(val + " ");
@@ -38,7 +35,7 @@ public class N과M {
             if (!visit[i]) {
                 visit[i] = true;
                 arr[depth] = i + 1;
-                dfs(N, M, depth + 1);
+                dfs(N, M, depth + 1, arr, visit);
                 visit[i] = false;
             }
         }
